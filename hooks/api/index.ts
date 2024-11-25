caaa2d9a38621a33f0b9f74c7d1b3583c8eab26a
닮 <dhdpekfa1@daum.net>
2024-11-25 14:05:09 +0900

    [refactor] supabase 내부 훅 폴더 별로 파일 분리

--- hooks/api/index.ts
+++ hooks/api/index.ts
@@ -1,17 +1,17 @@
 // Page Table
-export { useGetPageList } from "./supabase/useGetPageList";
-export { useGetPage } from "./supabase/useGetPage";
-export { useAddPage } from "./supabase/useAddPage";
-export { useUpdatePage } from "./supabase/useUpdatePage";
-export { useDeletePage } from "./supabase/useDeletePage";
+export { useGetPageList } from "./supabase/page/useGetPageList";
+export { useGetPage } from "./supabase/page/useGetPage";
+export { useAddPage } from "./supabase/page/useAddPage";
+export { useUpdatePage } from "./supabase/page/useUpdatePage";
+export { useDeletePage } from "./supabase/page/useDeletePage";
 
 // Board Table
-export { useGetBoardList } from "./supabase/useGetBoardList";
-export { useAddBoard } from "./supabase/useAddBoard";
-export { useUpdateBoard } from "./supabase/useUpdateBoard";
-export { useDeleteBoard } from "./supabase/useDeleteBoard";
+export { useGetBoardList } from "./supabase/board/useGetBoardList";
+export { useAddBoard } from "./supabase/board/useAddBoard";
+export { useUpdateBoard } from "./supabase/board/useUpdateBoard";
+export { useDeleteBoard } from "./supabase/board/useDeleteBoard";
 
 // Content Table
-export { useGetContent } from "./supabase/useGetContent";
-export { useAddContent } from "./supabase/useAddContent";
-export { useUpdateContent } from "./supabase/useUpdateContent";
+export { useGetContent } from "./supabase/content/useGetContent";
+export { useAddContent } from "./supabase/content/useAddContent";
+export { useUpdateContent } from "./supabase/content/useUpdateContent";
